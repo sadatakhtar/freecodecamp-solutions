af2127b42b0c8895c59b766610476c521b5dc068
Sadat Akhtar <shadowak47@yahoo.com>
2018-06-20 18:32:31 +0100

    Create objectLookup.js
    An example of object lookup

---
+++ objectLookup.js
@@ -0,0 +1,17 @@
+function searchObj(num){
+  var result = "";
+}
+
+var myObj = {
+
+  1: "dogs",
+  2: "cats",
+  3: "mice",
+  4: "fish",
+  5: "sheep",
+  6: "elephant",
+
+};
+
+var result = myObj[num];
+return result;
